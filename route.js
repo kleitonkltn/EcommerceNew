0b0ef57e3ddcfe57ab9f8da5066c7ed8b53ef383
kleitonkltn <kleitonkltn@gmail.com>
2018-09-28 14:55:52 -0300

    Menu/Categorias - Pronto

--- route.js
+++ route.js
@@ -1,6 +1,14 @@
 angular.module("Ecommerce").config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when("menu",{
+        controller: "menuController"
+    });
     $routeProvider.when("/home", {
         templateUrl: "view/home.html",
         controller: "homeController"
     });
+    $routeProvider.when("/categoryProducts/:idCategoria", {
+        templateUrl: "view/categoryProducts.html",
+        controller: "productsController"
+    });
+
 }]);
